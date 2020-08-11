120875655c1e1ca04bffa678652531deff6bfcf1
grrazzlle <cel47@miami.edu>
2020-08-10 21:54:49 -0400

    fixed save repeat by moving out of submit button function take 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let userSearch = '';
 const userDropdown = $('#audience-input').val();
 
 
-$('#form-button').click(function(event) {
+$('#form-button').click(function (event) {
   event.preventDefault();
   userSearch = $('#interest-input').val();
   console.log(userSearch);
@@ -19,44 +19,44 @@ $('#form-button').click(function(event) {
 
   })
 
-      .then(function(response) {
-        console.log(response);
-        $('.results-wrapper').empty();
-        for (let i = 0; i < response.data.length; i++) {
-          const interest = response.data[i].name;
-          const audience = response.data[i].audience_size;
-          let breakpoint = $('#audience-input').val();
-          console.log(breakpoint);
-          
+    .then(function (response) {
+      console.log(response);
+      $('.results-wrapper').empty();
+      for (let i = 0; i < response.data.length; i++) {
+        const interest = response.data[i].name;
+        const audience = response.data[i].audience_size;
+        let breakpoint = $('#audience-input').val();
+        console.log(breakpoint);
 
-          if (breakpoint == 1 && parseInt(audience) <= 50000) {
-            appendResults(interest, audience, i);
-          } else if (breakpoint == 2 && parseInt(audience) <= 100000 && parseInt(audience) > 50000) {
-            appendResults(interest, audience, i);
-          } else if (breakpoint == 3 && parseInt(audience) <= 500000 && parseInt(audience) > 100001) {
-            appendResults(interest, audience, i);
-          } else if (breakpoint == 4 && parseInt(audience) <= 1000000 && parseInt(audience) > 500001) {
-            appendResults(interest, audience, i);
-          } else if (breakpoint == 5 && parseInt(audience) <= 2000000 && parseInt(audience) > 1000001) {
-            appendResults(interest, audience, i);
-          }
-        };
-      });
-      function appendResults(interest, audience, i) {
-            const resultsCard = $('<div>').addClass('card results-card').attr('data-general', i);
-            const resultsInterest = $('<div>').addClass('results-interest').text(interest).attr('id', 'interest' + i);
-            const resultsAudience = $('<div>').addClass('results-Audience').text(audience).attr('id', 'audience' + i);
 
-            $('.results-wrapper').append(resultsCard);
-            resultsCard.append(resultsInterest).append(resultsAudience);
+        if (breakpoint == 1 && parseInt(audience) <= 50000) {
+          appendResults(interest, audience, i);
+        } else if (breakpoint == 2 && parseInt(audience) <= 100000 && parseInt(audience) > 50000) {
+          appendResults(interest, audience, i);
+        } else if (breakpoint == 3 && parseInt(audience) <= 500000 && parseInt(audience) > 100001) {
+          appendResults(interest, audience, i);
+        } else if (breakpoint == 4 && parseInt(audience) <= 1000000 && parseInt(audience) > 500001) {
+          appendResults(interest, audience, i);
+        } else if (breakpoint == 5 && parseInt(audience) <= 2000000 && parseInt(audience) > 1000001) {
+          appendResults(interest, audience, i);
+        }
+      };
+    });
+  function appendResults(interest, audience, i) {
+    const resultsCard = $('<div>').addClass('card results-card').attr('data-general', i);
+    const resultsInterest = $('<div>').addClass('results-interest').text(interest).attr('id', 'interest' + i);
+    const resultsAudience = $('<div>').addClass('results-Audience').text(audience).attr('id', 'audience' + i);
 
-            const saveBtn = $('<button>').addClass('save-btn ghost-btn').text('Save');
-            resultsCard.append(saveBtn);
-          }
+    $('.results-wrapper').append(resultsCard);
+    resultsCard.append(resultsInterest).append(resultsAudience);
 
+    const saveBtn = $('<button>').addClass('save-btn ghost-btn').text('Save');
+    resultsCard.append(saveBtn);
+  }
 });
-$(document).on('click', '.save-btn', function(event) {
-    let parentClone = $(this).parent().clone();
-    $('.saved-interests').append(parentClone);
-    $('.saved-interests .save-btn').remove();
-  });
+
+$(document).on('click', '.save-btn', function (event) {
+  let parentClone = $(this).parent().clone();
+  $('.saved-interests').append(parentClone);
+  $('.saved-interests .save-btn').remove();
+});
