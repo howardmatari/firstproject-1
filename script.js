3eb7f61be5ce0ec7a041ef05034485ceb6e3285e
grrazzlle <cel47@miami.edu>
2020-08-12 18:40:38 -0400

    added saved interest scroll

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 /* eslint-disable prefer-const */
 let userSearch = '';
 
-// create if/then statement for the audience size
-const userDropdown = $('#audience-input').val();
-
-
-$('#form-button').click(function (event) {
+$('#form-button').click(function(event) {
   event.preventDefault();
   userSearch = $('#interest-input').val();
   console.log(userSearch);
@@ -16,32 +12,29 @@ $('#form-button').click(function (event) {
   $.ajax({
     url: queryURL,
     method: 'GET',
-
   })
+      .then(function(response) {
+        console.log(response);
+        $('.results-wrapper').empty();
+        for (let i = 0; i < response.data.length; i++) {
+          const interest = response.data[i].name;
+          const audience = response.data[i].audience_size;
+          let breakpoint = $('#audience-input').val();
+          console.log(breakpoint);
 
-    .then(function (response) {
-      console.log(response);
-      $('.results-wrapper').empty();
-      for (let i = 0; i < response.data.length; i++) {
-        const interest = response.data[i].name;
-        const audience = response.data[i].audience_size;
-        let breakpoint = $('#audience-input').val();
-        console.log(breakpoint);
-
-
-        if (breakpoint == 1 && parseInt(audience) <= 50000) {
-          appendResults(interest, audience, i);
-        } else if (breakpoint == 2 && parseInt(audience) <= 100000 && parseInt(audience) > 50000) {
-          appendResults(interest, audience, i);
-        } else if (breakpoint == 3 && parseInt(audience) <= 500000 && parseInt(audience) > 100001) {
-          appendResults(interest, audience, i);
-        } else if (breakpoint == 4 && parseInt(audience) <= 1000000 && parseInt(audience) > 500001) {
-          appendResults(interest, audience, i);
-        } else if (breakpoint == 5 && parseInt(audience) <= 2000000 && parseInt(audience) > 1000001) {
-          appendResults(interest, audience, i);
-        }
-      };
-    });
+          if (breakpoint == 1 && parseInt(audience) <= 50000) {
+            appendResults(interest, audience, i);
+          } else if (breakpoint == 2 && parseInt(audience) <= 100000 && parseInt(audience) > 50000) {
+            appendResults(interest, audience, i);
+          } else if (breakpoint == 3 && parseInt(audience) <= 500000 && parseInt(audience) > 100001) {
+            appendResults(interest, audience, i);
+          } else if (breakpoint == 4 && parseInt(audience) <= 1000000 && parseInt(audience) > 500001) {
+            appendResults(interest, audience, i);
+          } else if (breakpoint == 5 && parseInt(audience) <= 2000000 && parseInt(audience) > 1000001) {
+            appendResults(interest, audience, i);
+          }
+        };
+      });
   function appendResults(interest, audience, i) {
     const resultsCard = $('<div>').addClass('card results-card').attr('data-general', i);
     const resultsInterest = $('<div>').addClass('results-interest').text(interest).attr('id', 'interest' + i);
@@ -55,7 +48,7 @@ $('#form-button').click(function (event) {
   }
 });
 
-$(document).on('click', '.save-btn', function (event) {
+$(document).on('click', '.save-btn', function(event) {
   let parentClone = $(this).parent().clone();
   $('.saved-interests').append(parentClone);
   $('.saved-interests .save-btn').remove();
